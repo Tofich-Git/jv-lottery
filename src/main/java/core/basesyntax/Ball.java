58d9781e710d10a9c35d2ef05454eeb0f289bd70
Yevhenii <evgeniyg0801@gmail.com>
2025-06-09 21:14:59 +0300

    fixed all errors

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,8 +1,8 @@
 package core.basesyntax;
 
 public class Ball {
-    private Color color;
-    private int number;
+    final Color color;
+    final int number;
 
     public Ball(Color color, int number) {
         this.color = color;
@@ -16,7 +16,6 @@ public class Ball {
     public int getNumber() {
         return number;
     }
-
     @Override
     public String toString() {
         return "Ball{color=" + color + ", number=" + number + "}";
